41e4e6ff01cd63d84ed6b366f908188cf44f0a50
rahmaevao <rahmaevao@gmail.com>
2021-05-07 10:36:31 +0300

    Исправил пути
    Так нужно для интеграции в IDE

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-start_cwd = os.getcwd()
+start_cwd = r'C:\Users\RahmaevAO\STM32CubeIDE\workspace_1.6.0\stm32CanBootloader\Core\versionPaster'
 os.chdir(r'C:\Users\RahmaevAO\STM32CubeIDE\workspace_1.6.0\stm32CanBootloader')
 
 version_string = subprocess.check_output(["git", "describe", "--always"]).strip().decode()
@@ -17,6 +17,6 @@ print('Version name:', version_string)
 os.chdir(start_cwd)
 f = open('version.h', 'w')
 f.write('// This file contains the version of the last commit.\n')
-f.write('// This file is generated automatically by the \n// '+ start_cwd + 'main.py' + ' script.\n')
+f.write('// This file is generated automatically by the \n// '+ start_cwd + '\main.py' + ' script.\n')
 f.write('// This script should be run before building.\n\n')
-f.write('#define VERSION ' + version_string)
+f.write('#define VERSION "' + version_string + '"')
--- version.h
+++
@@ -1,6 +0,0 @@
-// This file contains the version of the last commit.
-// This file is generated automatically by the 
-// C:\Users\RahmaevAO\PycharmProjects\versionPastermain.py script.
-// This script should be run before building.
-
-#define VERSION c7b2825
